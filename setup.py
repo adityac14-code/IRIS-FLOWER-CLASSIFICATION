84d6e31b71b117a9a314b7ea6f2e5590f0277a6c
adityac14-code <121824546+adityac14-code@users.noreply.github.com>
2023-11-28 23:54:37 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='=iris-flower-classifier',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'scikit-learn'
+        
+    ],
+    author='Chitte Aditya',
+    author_email='chitteaditya1@gmail.com',
+    description='Iris Flower Classification',
+    url='https://github.com/adityac14-code/IRIS-FLOWER-CLASSIFICATION',
+    
+)
